dcc3e0e5578973a978fa3fad49cc5da9a254847f
fairuscmancilla-hash <fairuscmancilla@gmail.com>
2026-09-16 16:50:56 -0500

    Formulario agrega productos a lista observable

---
+++ Semana 04/Cerron/lab04/Lab04CarritoTecsup/app/src/main/java/com/cerron/lab04carritotecsup/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.cerron.lab04carritotecsup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cerron.lab04carritotecsup.ui.theme.Lab04CarritoTecsupTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.unit.dp
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            Lab04CarritoTecsupTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PantallaCarrito(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PantallaCarrito(modifier: Modifier = Modifier) {
+
+    var nombre by remember { mutableStateOf("") }
+    var precio by remember { mutableStateOf("") }
+    var cantidad by remember { mutableStateOf("") }
+
+    val productos = remember {
+        mutableStateListOf<Producto>()
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+
+        Text(text = "Mi Carrito TECSUP")
+
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = { nombre = it },
+            label = { Text("Nombre del producto") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+
+            OutlinedTextField(
+                value = precio,
+                onValueChange = { precio = it },
+                label = { Text("Precio") },
+                modifier = Modifier.weight(1f)
+            )
+
+            OutlinedTextField(
+                value = cantidad,
+                onValueChange = { cantidad = it },
+                label = { Text("Cantidad") },
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Button(
+            onClick = {
+                val precioNum = precio.toDoubleOrNull() ?: 0.0
+                val cantidadNum = cantidad.toIntOrNull() ?: 0
+
+                if (
+                    nombre.isNotBlank() &&
+                    precioNum > 0 &&
+                    cantidadNum > 0
+                ) {
+                    productos.add(
+                        Producto(
+                            nombre = nombre,
+                            precio = precioNum,
+                            cantidad = cantidadNum
+                        )
+                    )
+
+                    nombre = ""
+                    precio = ""
+                    cantidad = ""
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("AGREGAR")
+        }
+
+        Text(
+            text = "Productos: ${productos.size}"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PantallaCarritoPreview() {
+    Lab04CarritoTecsupTheme {
+        PantallaCarrito()
+    }
+}
---
+++ Semana 04/Cerron/lab04/Lab04CarritoTecsup/app/src/main/java/com/cerron/lab04carritotecsup/Producto.kt
@@ -0,0 +1,7 @@
+package com.cerron.lab04carritotecsup
+
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    val cantidad: Int
+)
