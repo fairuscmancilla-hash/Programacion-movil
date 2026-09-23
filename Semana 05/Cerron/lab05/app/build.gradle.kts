e53caadecb2f74cb81da764f140de0fd29e0ae80
Yajaira Cerron <fairus.cerron@tecsup.edu.pe>
2026-09-23 08:33:12 -0500

    Lab05: crear proyecto base con dependencia navigation-compose 2.7.7

---
+++ Semana 05/Cerron/lab05/app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.example.semana05_navegacion"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.semana05_navegacion"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    // ← Navegación (dependencia del laboratorio)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
---
+++ Semana 05/Cerron/lab05/app/src/main/java/com/example/semana05_navegacion/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.example.semana05_navegacion.ui.theme
+
+import androidx.compose.material3.Typography
+
+val Typography = Typography()
