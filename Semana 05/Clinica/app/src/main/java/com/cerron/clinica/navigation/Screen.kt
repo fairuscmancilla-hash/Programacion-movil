46415952f745751f9a99d284fdd2c263be397c39
Yajaira Cerron <fairus.cerron@tecsup.edu.pe>
2026-09-23 14:56:12 -0500

    Agrega modelo de datos y rutas de navegacion

---
+++ Semana 05/Clinica/app/src/main/java/com/cerron/clinica/navigation/Screen.kt
@@ -0,0 +1,40 @@
+package com.cerron.clinica.navigation
+
+sealed class Screen(val route: String) {
+
+    // Pantalla principal
+    object Home : Screen("home")
+
+    // Perfil del médico seleccionado
+    object PerfilMedico : Screen("perfil_medico/{medicoId}") {
+        fun crearRuta(medicoId: Int): String {
+            return "perfil_medico/$medicoId"
+        }
+    }
+
+    // Pantalla para seleccionar fecha y hora
+    object AgendarCita : Screen("agendar_cita/{medicoId}") {
+        fun crearRuta(medicoId: Int): String {
+            return "agendar_cita/$medicoId"
+        }
+    }
+
+    // Pantalla final con el resumen de la cita
+    object Confirmacion :
+        Screen("confirmacion/{medicoId}/{fecha}/{hora}") {
+
+        fun crearRuta(
+            medicoId: Int,
+            fecha: String,
+            hora: String
+        ): String {
+            return "confirmacion/$medicoId/$fecha/$hora"
+        }
+    }
+
+    // Pantalla del menú lateral
+    object MisCitas : Screen("mis_citas")
+
+    // Pantalla del menú lateral
+    object Historial : Screen("historial")
+}
